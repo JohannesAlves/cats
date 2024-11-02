7925b5040c13d1dcefbb4d644c7441038eb6f388
johannesalves <johannes001002@gmail.com>
2024-11-02 14:42:02 -0300

    chore: implement the grid of list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
     <main className="flex justify-center flex-col items-center">
       <h1 className="text-6xl font-bold mt-10">CATKNOW</h1>
 
-      <section className="grid grid-cols-4 gap-10 p-10">
+      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 p-10">
         {cats.map((cat) => (
           <Card
             key={`cat-${cat.id}`}
