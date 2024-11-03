544684ef71a7f6789c24ab4a6fc33ce48895ad43
johannesalves <johannes001002@gmail.com>
2024-11-02 21:00:20 -0300

    chore: change the title of app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Many Cats.',
+  title: 'CATSKNOW',
   description: 'A cats app.',
 };
 
